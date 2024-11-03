8ab535fe7777e8853820e27887d825e14537231a
bryan <bryanvivanco97@outlook.com>
2024-11-03 12:16:34 -0500

    añade peticiones put y delete

--- routers/users_db.py
+++ routers/users_db.py
@@ -2,8 +2,9 @@
 
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
-from db.schemas.user import user_schema
+from db.schemas.user import user_schema,users_schema
 from db.client import db_client
+from bson import ObjectId
 
 
 router = APIRouter(prefix="/usersdb",
@@ -12,7 +13,7 @@ router = APIRouter(prefix="/usersdb",
                 )
 
 #funcion para buscar un usuario creado
-def search_user(field,key):
+def search_user(field: str ,key):
     try:
         user= db_client.local.users.find_one({field:key})
         return User(**(user_schema(user)))
@@ -20,12 +21,24 @@ def search_user(field,key):
         return {"error":"No se ha encontrado el usuario"}
 
     
-### Peticiones
+### PETICIONES
 
-@router.get("/")
+# PETICION PARA BUSCAR USUARIOS
+
+@router.get("/",response_model=list[User]) #Buscar todos 
 async def users_db():
-    return " "
+    return users_schema(db_client.local.users.find())
 
+@router.get("/{id}") #Path
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
+
+@router.get("/") #Query
+async def user(id:str):
+    return search_user("_id",ObjectId(id))
+
+
+# PETICION PARA AGREGAR USUARIO A DB
 
 @router.post("/", response_model=User,status_code=status.HTTP_201_CREATED)
 async def user(user: User):
@@ -43,3 +56,28 @@ async def user(user: User):
     new_user = user_schema(db_client.local.users.find_one({"_id":id})) # hacemos la transformacion en el formato para devolverlo despues de encontrarlo por id
     
     return User(**new_user)
+
+# PETICION PARA ELIMINAR USUARIOS
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def user(id: str):
+    
+    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    
+    if not found:
+        return {"error": "No se ha eliminado el usuario"}
+    
+# PETICION PARA ACTUALIZAR USUARIO
+
+@router.put("/", response_model=User)
+async def user(user: User):
+    
+    user_dict= dict(user) #crear formato dict para user
+    del user_dict["id"] #eliminar campo id para que lo genere automaticamente mongoDB
+    
+    try:
+        db_client.local.users.find_one_and_replace({"_id":ObjectId(user.id)},user_dict)
+    except:
+        return {"error": "No se ha actualizado el usuario"}
+    
+    return search_user("_id",ObjectId(user.id))
